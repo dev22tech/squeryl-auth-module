c792625da9ecf50cfc2e008c974da6bc29e42b26
Armando Orellana <armando.orellana@22-tech.com>
2016-07-04 17:24:37 -0400

    Updating the scalatest version

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object LiftModuleBuild extends Build {
         "net.liftweb" %% "lift-squeryl-record" % liftVersion % "provided",
         "net.liftweb" %% "lift-webkit" % liftVersion % "provided",
         "ch.qos.logback" % "logback-classic" % "1.0.3" % "provided",
-        "org.scalatest" %% "scalatest" % scalaTestVer % "test",
+        "org.scalatest" %% "scalatest" % "1.9.2" % "test",
         "org.mindrot" % "jbcrypt" % "0.3m" % "compile",
         "joda-time" % "joda-time" % "2.3" % "provided",
         "com.h2database" % "h2" % "1.3.167"
